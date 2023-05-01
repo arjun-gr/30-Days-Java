1bdcb483d90ed701fb9069def803cdd4556d9d81
xeno <arjungr8898@gmail.com>
2023-05-01 19:03:14 +0530

    day 0

---
+++ Day 0/Hello.java
@@ -0,0 +1,6 @@
+public class Hello {
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to 30 Days of Code!");
+    }
+}
