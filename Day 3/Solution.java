ddd6bf3dce1266d740027d38fd5033f9d40190d9
Arjun Gr <81943867+arjun-gr@users.noreply.github.com>
2023-05-08 18:42:39 +0530

    Update Solution.java

--- Day 3/Solution.java
+++ Day 3/Solution.java
@@ -8,9 +8,9 @@ public class Solution {
 
         if (N % 2 != 0) {
             System.out.println("Weird");
-        } else if (N % 2 == 0 && N > 2 && N < 5) {
+        } else if (N % 2 == 0 && N >= 2 && N <= 5) {
             System.out.println("Not Weird");
-        } else if (N % 2 == 0 && N > 6 && N < 20) {
+        } else if (N % 2 == 0 && N >= 6 && N <= 20) {
             System.out.println("Weird");
         } else {
             System.out.println("Not Weird");
