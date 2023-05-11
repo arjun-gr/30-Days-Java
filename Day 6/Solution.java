0bc04bd46177855f57c0520dda1bf8a2c8236dd2
xeno <arjungr8898@gmail.com>
2023-05-11 22:56:50 +0530

    day 6 Revision of all the 5 days

---
+++ Day 6/Solution.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < T; i++) {
+            String S = sc.nextLine();
+            evenOddChars(S);
+        }
+    }
+
+    public static void evenOddChars(String S) {
+        StringBuilder even = new StringBuilder();
+        StringBuilder odd = new StringBuilder();
+
+        for (int i = 0; i < S.length(); i++) {
+            if (i % 2 == 0 || i == 0) {
+                even.append(S.charAt(i));
+            } else {
+                odd.append(S.charAt(i));
+            }
+        }
+        System.out.println(even.toString() + " " + odd.toString());
+    }
+}
